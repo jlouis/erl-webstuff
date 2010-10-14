43ae635ce427d874dfb3b1f9ed105cfe0024cdd4
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-10-15 00:04:10 +0200

    Flesh out the hier part.

--- src/uri.erl
+++ src/uri.erl
@@ -10,9 +10,13 @@
 -export([mk_uri/2, mk_uri/3, mk_uri/4,
 	 uri_to_iolist/1, uri_to_string/1]).
 
+-record(hier,
+	{authority :: string() | none,
+	 path      :: string() | none}).
+
 -record(uri,
 	{scheme :: atom(),
-	 hier :: string(),
+	 hier :: #hier{},
 	 q :: [{string(), string()}],
 	 fragment :: string() | none}).
 
@@ -30,8 +34,8 @@ mk_uri(S,H,Q,F) ->
 
 -spec uri_to_iolist(#uri{}) -> iolist().
 uri_to_iolist(#uri{scheme = S, hier = H, q = Q, fragment = F}) ->
-    [atom_to_list(S),
-     H,
+    [atom_to_list(S),":",
+     hier_to_iolist(H),
      query_to_iolist(Q),
      fragment_to_iolist(F)].
 
@@ -42,6 +46,14 @@ uri_to_string(Uri) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+hier_to_iolist(#hier{authority = A, path = P}) ->
+    case {A,P} of
+	{none, none} -> "";
+	{none, P1} -> P1;
+	{A1, none} -> ["//", A1];
+	{A1, P1} ->   ["//", A1, "/", P1]
+    end.
+
 query_to_iolist([]) ->
     "";
 query_to_iolist(L) ->
