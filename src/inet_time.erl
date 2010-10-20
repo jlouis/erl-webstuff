f888a111517576a8fad907aaca95e9d29a684f2f
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-10-20 14:30:09 +0200

    Improve the RFC3339 parser: Validate parse.
    ; We are not yet handling leap seconds.
    ; We lack extensive testing.

--- src/inet_time.erl
+++ src/inet_time.erl
@@ -15,9 +15,33 @@
 %%====================================================================
 parse(String) ->
     {ok, Tokens, _} = inet_time_scanner:string(String),
-    inet_time_parser_simple:parse(Tokens).
+    {ok, {Date, {Time, Offset}}} = inet_time_parser_simple:parse(Tokens),
+    case calendar:valid_date(Date) of
+	true ->
+	    case valid_time(Time) of
+		true -> {ok, {Date, Time, Offset}};
+		false -> {error, {invalid_time, {Date, Time, Offset}}};
+		leap_second -> {error, no_leap_second_handling}
+	    end;
+	false ->
+	    {error, {invalid_date, {Date, Time, Offset}}}
+    end.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+valid_time({Hour, Minute, Second, _Frac})
+  when 0 =< Hour,
+       Hour =< 23,
+       0 =< Minute,
+       Minute =< 59,
+       0 =< Second,
+       Second =< 60 ->
+    case Second of
+	60 -> leap_second;
+	_  -> true
+    end;
+valid_time(_Otherwise) -> false.
+
+
 
