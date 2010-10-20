08dbb287a66c3d117df52ede216056f7ccbb63b6
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2010-10-20 15:42:47 +0200

    Provide a test case for the 'Z' style encoding and one for Sep 1752.

--- test/inet_time_tests.erl
+++ test/inet_time_tests.erl
@@ -7,4 +7,8 @@ parse_test() ->
 	inet_time:parse("1984-02-29T23:20:50.52+8:0"),
     {error, {invalid_date,{{1984,2,30},{23,20,50,52},{'+',8,0}}}} =
 	inet_time:parse("1984-02-30T23:20:50.52+8:0"),
+    {ok,{{1753,9,7},{1,0,0,0},'Z'}} =
+	inet_time:parse("1753-9-7T01:00:00Z"),
+    {error, {invalid_date,{{1752,9,7},{1,0,0,0},'Z'}}} =
+	inet_time:parse("1752-9-7T01:00:00Z"),
     done.
